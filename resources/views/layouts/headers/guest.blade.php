83b7bc916e0424cdbed6f58a953b6a8afe0c266c
Afiq <zephyr.afiq@gmail.com>
2020-07-22 04:31:39 +0800

    update

--- resources/views/layouts/headers/guest.blade.php
+++ resources/views/layouts/headers/guest.blade.php
@@ -1,9 +1,9 @@
 <div class="header bg-gradient-primary py-7 py-lg-8">
     <div class="container">
-        <div class="header-body text-center mb-7">
+        <div class="header-body text-center mt-7 mb-7">
             <div class="row justify-content-center">
                 <div class="col-lg-5 col-md-6">
-                    <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
+                    <h1 class="text-white">{{ __('Welcome to Face Attendance System Live Preview.') }}</h1>
                 </div>
             </div>
         </div>
